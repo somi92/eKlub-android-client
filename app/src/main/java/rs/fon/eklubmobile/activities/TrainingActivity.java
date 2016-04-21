27ad26fded7b7b38d0e31ffe31f770144aa4892a
Milos Stojanovic <milos92s@yahoo.com>
2016-04-21 21:26:30 +0200

    Refactored packages

--- app/src/main/java/rs/fon/eklubmobile/activities/TrainingActivity.java
+++ app/src/main/java/rs/fon/eklubmobile/activities/TrainingActivity.java
@@ -1,7 +1,9 @@
-package rs.fon.eklubmobile;
+package rs.fon.eklubmobile.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import rs.fon.eklubmobile.R;
 
 public class TrainingActivity extends AppCompatActivity {
 
