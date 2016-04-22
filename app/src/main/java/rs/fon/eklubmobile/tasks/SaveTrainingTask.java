7bbb9a219ebb624be2933c71948607e1a5486a6d
Milos Stojanovic <milos92s@yahoo.com>
2016-04-22 22:20:55 +0200

    Setting up tasks

---
+++ app/src/main/java/rs/fon/eklubmobile/tasks/SaveTrainingTask.java
@@ -0,0 +1,24 @@
+package rs.fon.eklubmobile.tasks;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by milos on 4/22/16.
+ */
+public class SaveTrainingTask extends AsyncTask<String, Integer, Boolean> {
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Boolean doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean aBoolean) {
+        super.onPostExecute(aBoolean);
+    }
+}
