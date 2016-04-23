6cb61792cab4ed51725c005b6c9012db49330385
Milos Stojanovic <milos92s@yahoo.com>
2016-04-24 00:02:46 +0200

    Tested GetAllGroupsTask

--- app/src/main/java/rs/fon/eklubmobile/activities/TrainingActivity.java
+++ app/src/main/java/rs/fon/eklubmobile/activities/TrainingActivity.java
@@ -2,18 +2,29 @@ package rs.fon.eklubmobile.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import org.json.JSONObject;
 
 import rs.fon.eklubmobile.R;
 import rs.fon.eklubmobile.listeners.EKlubEventListener;
+import rs.fon.eklubmobile.tasks.GetAllGroupsTask;
 
 public class TrainingActivity extends AppCompatActivity implements EKlubEventListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training);
+        loadGroups();
+    }
+
+    private void loadGroups() {
+        String url = "192.168.1.181:8080";
+        GetAllGroupsTask groupsTask = new GetAllGroupsTask(this);
+        groupsTask.execute(url);
     }
 
     @Override
@@ -23,7 +34,12 @@ public class TrainingActivity extends AppCompatActivity implements EKlubEventLis
 
     @Override
     public void onDataReceived(JSONObject data) {
+        try {
+            String group = data.getJSONArray("payload").getJSONObject(0).toString();
+            Toast.makeText(this, group.toString(), Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
 
+        }
     }
 
     @Override
@@ -33,6 +49,6 @@ public class TrainingActivity extends AppCompatActivity implements EKlubEventLis
 
     @Override
     public void onNotificationReceived(String message) {
-
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
