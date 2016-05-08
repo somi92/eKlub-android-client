a67225572f9380e0872b8f3fd4ff1c5c03260640
Milos Stojanovic <milos92s@yahoo.com>
2016-05-08 16:01:28 +0200

    Fixed date time picker

--- app/src/main/java/rs/fon/eklubmobile/activities/TrainingActivity.java
+++ app/src/main/java/rs/fon/eklubmobile/activities/TrainingActivity.java
@@ -1,5 +1,6 @@
 package rs.fon.eklubmobile.activities;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,7 @@ import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 import org.json.JSONObject;
 
 import java.util.Calendar;
+import java.util.Date;
 
 import rs.fon.eklubmobile.R;
 import rs.fon.eklubmobile.listeners.EKlubEventListener;
@@ -43,7 +45,8 @@ public class TrainingActivity extends AppCompatActivity implements EKlubEventLis
                         now.get(Calendar.MONTH),
                         now.get(Calendar.DAY_OF_MONTH)
                 );
-                dpd.setThemeDark(true);
+//                dpd.setThemeDark(true);
+                dpd.setAccentColor(Color.BLUE);
                 dpd.show(getFragmentManager(), "Datepickerdialog");
             }
         });
@@ -55,13 +58,14 @@ public class TrainingActivity extends AppCompatActivity implements EKlubEventLis
                 TimePickerDialog tpd = TimePickerDialog.newInstance(
                         TrainingActivity.this, 12, 12, 30, true
                 );
-                tpd.setThemeDark(true);
+//                tpd.setThemeDark(true);
+                tpd.setAccentColor(Color.BLUE);
                 tpd.show(getFragmentManager(), "Timepickerdialog");
             }
         });
 
         mDateTime = (TextView) findViewById(R.id.lblDateTime);
-
+        initializeDateTime();
     }
 
     private void loadGroups() {
@@ -97,17 +101,41 @@ public class TrainingActivity extends AppCompatActivity implements EKlubEventLis
 
     @Override
     public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
-        String date = "You picked the following date: "+dayOfMonth+"/"+(monthOfYear+1)+"/"+year;
-        Toast.makeText(this, date, Toast.LENGTH_LONG).show();
-        String selectedDate = dayOfMonth + "/" + monthOfYear + "/" + year;
-        mDateTime.setText(selectedDate);
+//        String date = "You picked the following date: "+dayOfMonth+"/"+(monthOfYear+1)+"/"+year;
+//        Toast.makeText(this, date, Toast.LENGTH_LONG).show();
+        String monthOfYearString = (monthOfYear + 1) < 10 ? "0" + (monthOfYear + 1)  : (monthOfYear + 1) + "";
+        String dayOfMonthString = dayOfMonth < 10 ? "0" + dayOfMonth : dayOfMonth + "";
+        String currentDateTime = mDateTime.getText().toString();
+        String selectedDate = year + "-" + monthOfYearString + "-" + dayOfMonthString;
+        currentDateTime = currentDateTime.replace(currentDateTime.substring(0, currentDateTime.indexOf(" ")), selectedDate);
+        mDateTime.setText(currentDateTime);
     }
 
     @Override
     public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int second) {
-        String time = "You picked the following time: "+hourOfDay+"h"+minute;
-        Toast.makeText(this, time, Toast.LENGTH_LONG).show();
-        String selectedTime = hourOfDay + ":" + minute;
-        mDateTime.setText(mDateTime.getText() + " " + selectedTime);
+//        String time = "You picked the following time: "+hourOfDay+"h"+minute;
+//        Toast.makeText(this, time, Toast.LENGTH_LONG).show();
+        String hourOfDayString = hourOfDay < 10 ? "0" + hourOfDay : hourOfDay + "";
+        String minuteString = minute < 10 ? "0" + minute : minute + "";
+        String selectedTime = hourOfDayString + ":" + minuteString;
+        String currentDateTime = mDateTime.getText().toString();
+        currentDateTime = currentDateTime.replace(currentDateTime.substring(currentDateTime.indexOf(" ") + 1), selectedTime);
+        mDateTime.setText(currentDateTime);
+    }
+
+    private void initializeDateTime() {
+        int monthOfYear = Calendar.getInstance().get(Calendar.MONTH);
+        int dayOfMonth = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        int hourOfDay = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        int minute = Calendar.getInstance().get(Calendar.MINUTE);
+        String monthOfYearString = (monthOfYear + 1) < 10 ? "0" + (monthOfYear + 1)  : (monthOfYear + 1) + "";
+        String dayOfMonthString = dayOfMonth < 10 ? "0" + dayOfMonth : dayOfMonth + "";
+        String hourOfDayString = hourOfDay < 10 ? "0" + hourOfDay : hourOfDay + "";
+        String minuteString = minute < 10 ? "0" + minute : minute + "";
+        mDateTime.setText(Calendar.getInstance().get(Calendar.YEAR)
+                + "-" + monthOfYearString
+                + "-" + dayOfMonthString
+                + " " + hourOfDayString
+                + ":" + minuteString);
     }
 }
