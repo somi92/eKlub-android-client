c53f10b9b432efdf5e4afe713df3fb6fd04d5aa2
Milos Stojanovic <milos92s@yahoo.com>
2016-06-12 19:19:37 +0200

    Fixed attendance list duplication issue

--- app/src/main/java/rs/fon/eklubmobile/activities/AttendancesActivity.java
+++ app/src/main/java/rs/fon/eklubmobile/activities/AttendancesActivity.java
@@ -1,9 +1,19 @@
 package rs.fon.eklubmobile.activities;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -16,12 +26,14 @@ import java.util.List;
 import rs.fon.eklubmobile.R;
 import rs.fon.eklubmobile.listeners.EKlubEventListener;
 import rs.fon.eklubmobile.tasks.GetMembersTask;
-import rs.fon.eklubmobile.util.AttendanceListAdapter;
 
 public class AttendancesActivity extends AppCompatActivity implements EKlubEventListener {
 
     private ListView mAttendancesListView;
     private List<JSONObject> mAttendances;
+
+    private String[] mTempEditText;
+    private boolean[] mTempIsAttendant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +81,8 @@ public class AttendancesActivity extends AppCompatActivity implements EKlubEvent
     }
 
     private void populateAttendanceList(JSONArray data) throws JSONException {
+        mTempEditText = new String[data.length()];
+        mTempIsAttendant = new boolean[data.length()];
         mAttendances = new ArrayList<>();
         for(int i=0; i<data.length(); i++) {
             JSONObject member = data.getJSONObject(i);
@@ -78,7 +92,89 @@ public class AttendancesActivity extends AppCompatActivity implements EKlubEvent
             attendance.put("isAttendant", true);
             mAttendances.add(attendance);
         }
-        ArrayAdapter<JSONObject> attendancesAdapter = new AttendanceListAdapter(this, mAttendances);
+        AttendanceListAdapter attendancesAdapter = new AttendanceListAdapter();
         mAttendancesListView.setAdapter(attendancesAdapter);
+    }
+
+    private class AttendanceListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            if(mAttendances != null && mAttendances.size() != 0){
+                return mAttendances.size();
+            }
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return mAttendances.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            JSONObject obj = mAttendances.get(position);
+            final ViewHolder holder;
+            if(convertView == null) {
+                holder = new ViewHolder();
+                convertView = LayoutInflater.from(AttendancesActivity.this).inflate(R.layout.attendance_item, parent, false);
+                holder.textView = (TextView) convertView.findViewById(R.id.lblMember);
+                holder.editText = (EditText) convertView.findViewById(R.id.txtLateMin);
+                holder.checkBox = (CheckBox) convertView.findViewById(R.id.chbIsAttendant);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.position = position;
+
+            try {
+                holder.textView.setText(obj.getJSONObject("member").getString("nameSurname"));
+                holder.editText.setText(mTempEditText[position]);
+                holder.checkBox.setChecked(mTempIsAttendant[position]);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            holder.editText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+                    mTempEditText[holder.position] = editable.toString();
+                }
+            });
+
+            holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    mTempIsAttendant[holder.position] = b;
+                }
+            });
+
+            return convertView;
+        }
+    }
+
+    private class ViewHolder {
+        TextView textView;
+        EditText editText;
+        CheckBox checkBox;
+        int position;
     }
 }
