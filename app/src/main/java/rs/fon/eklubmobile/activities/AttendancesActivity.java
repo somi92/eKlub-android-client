e244915b0e2b27fc53ebe270ebde581ebe3fef47
Milos Stojanovic <milos92s@yahoo.com>
2016-06-12 13:25:41 +0200

    Started attendances activity implementation

---
+++ app/src/main/java/rs/fon/eklubmobile/activities/AttendancesActivity.java
@@ -0,0 +1,15 @@
+package rs.fon.eklubmobile.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rs.fon.eklubmobile.R;
+
+public class AttendancesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendances);
+    }
+}
